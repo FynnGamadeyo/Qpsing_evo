02146cd56ecdb0ad028506dac77418c6ae0b18b4
Fynn <no@email.kr>
2014-09-25 22:22:28 +0200

    render 0.2

--- src/drivers/graphics/terminalRender.cc
+++ src/drivers/graphics/terminalRender.cc
@@ -4,30 +4,44 @@ TerminalRender::TerminalRender(){
   
 }
 
-void TerminalRender::render(WINDOW * w, vector<char> imagePix, int windowWidth, int windowHeight,int imagePosX,int imagePosY,int startDepth, int width, int height){
-	int endY;
-	if((imagePosY+windowHeight)>height){
-	  endY=(imagePosY+windowHeight)-((imagePosY+windowHeight)-width);
-	}else{
-	  endY=(imagePosY+windowHeight);
+void TerminalRender::render(WINDOW * w, vector<char> imagePix, int imageWidth, int imageHeight,int imagePosX,int imagePosY,int startDepth, int windowWidth, int windowHeight){
+	
+	if(imagePosX<windowWidth && imagePosY<windowHeight){
+  
+	int endY=(imagePosY+imageHeight);
+	int endX=(imagePosX+imageWidth);
+	int startX=imagePosX;
+	int startY=imagePosY;
+  
+	if(imagePosX<0){
+	  startX=0;
 	}
 	
-	int endX;
-	if((imagePosX+windowWidth)>width){
-	  endX=23232323; // <-<>->
-	}else{
-	  
+	if(imagePosY<0){
+	  startY=0;
+	}
+	
+	if((imagePosY+imageHeight)>windowHeight){
+	  endY=(imagePosY+imageHeight)-((imagePosY+imageHeight)-windowHeight);
+	}
+	
+	if((imagePosX+imageWidth)>windowWidth){
+	  endX=(imagePosX+imageWidth)-((imagePosX+imageWidth)-windowWidth); 
 	}
   
 	for(int y=imagePosY;y<endY;y++){
-	  for(int x=imagePosX;x<(imagePosX+windowWidth);x++){
+	  for(int x=imagePosX;x<(imagePosX+imageWidth);x++){
 	    
-	      mvwprintw(w,y,x,&imagePix[x + width * y + startDepth * width * height]);
+	      mvwprintw(w,y,x,&imagePix[x + windowWidth * y + startDepth * windowWidth * windowHeight]);
 	    
 	  }
 	}
 	wmove(w,0,0);
 	wrefresh(w);
+	
+	}else{
+	  // ERROR IMAGE IS OUT OF WINDOW !
+	}
   
 // 	for(int y=0;y<height;y++){
 // 		for(int x=0;x<width;x++){
